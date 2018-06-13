276d0a8af6e5de1ab1dbb6b095ad6f8f03fbafa0
Omar Alberto <omar_alberto_h@yahoo.es>
2018-06-13 00:40:16 -0500

    cambio a egr

--- egr.php
+++ egr.php
@@ -16,7 +16,7 @@ else    {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Wisdev-Administrador ISP</title>
+	<title>Wisdev-Administrador ISP...</title>
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">    
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto:300,400,500" rel="stylesheet">
 	<link rel="stylesheet" href="bower_components/DataTables/media/css/jquery.dataTables.min.css">
