b7f1dea9809d8f09f1e15954e43da7c7467f1769
Omar Alberto <omar_alberto_h@yahoo.es>
2018-06-14 00:51:21 -0500

    set correct time zone

--- fetch_payFact.php
+++ fetch_payFact.php
@@ -14,6 +14,7 @@ if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 	}	
 mysqli_set_charset($mysqli,"utf8");
+date_default_timezone_set('America/Bogota');
 $today = date("Y-m-d");   
 $convertdate= date("d-m-Y" , strtotime($today));
 $hourMin = date('H:i');
